a6cba5abd801a1a2788295881c8d46ffb76ba0bc
Eric Schmidt <e.ryan.schmidt@gmail.com>
2026-06-30 12:40:35 -0600

    Remove additional location declarations from src/App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,6 @@ import { useSDK } from '@contentful/react-apps-toolkit';
 const ComponentLocationSettings = {
   [locations.LOCATION_APP_CONFIG]: ConfigScreen,
   [locations.LOCATION_ENTRY_FIELD]: Field,
-  [locations.LOCATION_ENTRY_EDITOR]: EntryEditor,
-  [locations.LOCATION_DIALOG]: Dialog,
-  [locations.LOCATION_ENTRY_SIDEBAR]: Sidebar,
-  [locations.LOCATION_PAGE]: Page,
-  [locations.LOCATION_HOME]: Home,
 };
 
 const App = () => {
